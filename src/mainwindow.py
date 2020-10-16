1b58b46fc787fe6d4493d6bf3f0082e87fc466e7
Ross Wardrup <minorsecond@gmail.com>
2020-10-16 12:39:02 -0500

    Add ID to attribute options

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -14,31 +14,29 @@ class MainWindow(QtWidgets.QMainWindow, ui_mainwindow.Ui_MainWindow):
         self.setFixedSize(632, 300)
 
         self.exif_attributes = {
-            "Camera Manufacturer": '0x010f',
-            "Camera Model": '0x0110',
-            "Camera Orientation": '0x0112',
-            "Image X Resolution": '0x011a',
-            "Image Y Resolution": '0x011b',
-            "Exposure Time": '0x829a',
-            "FD Number": '0x829d',
-            "ISO": '0x8827',
-            "Date & Time Created": '0x9003',
-            "Aperture Value": '0x9202',
-            "Brightness Value": '0x9203',
-            "Exposure Compensation": '0x9204',
-            "Metering Mode": '0x9207',
-            "Image Number": '0x9211',
-            "Exposure Mode": '0xa402',
-            "White Balance Mode": '0xa403',
-            "Camera Serial Number": '0xa431',
-            "Lens Make": '0xa433',
-            "Lens Model": '0xa434',
-            "Lens Serial Number": '0xa435',
+            "Camera make {cmake}": '0x010f',
+            "Camera Model {cmodel}": '0x0110',
+            "Camera Orientation {corient}": '0x0112',
+            "Image X Resolution {xres}": '0x011a',
+            "Image Y Resolution {yres}": '0x011b',
+            "Exposure Time {etime}": '0x829a',
+            "FD Number {fdnum}": '0x829d',
+            "ISO {iso}": '0x8827',
+            "Date & Time Created {dtime}": '0x9003',
+            "Aperture Value {aperval}": '0x9202',
+            "Brightness Value {bright}": '0x9203',
+            "Exposure Compensation {excomp}": '0x9204',
+            "Metering Mode {meter}": '0x9207',
+            "Image Number {inum}": '0x9211',
+            "Exposure Mode {exmode}": '0xa402',
+            "White Balance Mode {wb}": '0xa403',
+            "Camera Serial Number {cserial}": '0xa431',
+            "Lens Make {lmake}": '0xa433',
+            "Lens Model {lmodel}": '0xa434',
+            "Lens Serial Number {lserial}": '0xa435',
         }
 
         self.exif_attributes = {key: value for key, value in sorted(self.exif_attributes.items())}
-
-        print(self.exif_attributes)
 
         for attribute in self.exif_attributes:
             self.attributeSelectorInput.addItem(attribute)
